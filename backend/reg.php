131ea44f0cdd394faaf2499a8f48c0da20c0765d
naczea <nico.acz21@gmail.com>
2020-07-09 20:36:56 -0500

    Chekin & login validation

--- backend/reg.php
+++ backend/reg.php
@@ -10,12 +10,40 @@
         $phone = $_POST['phone_user'];
         $pass = $_POST['pass_user'];
         $pass_cod = password_hash($pass,PASSWORD_DEFAULT);
+        
+        if(strlen($id) != 10 ){
+            echo 406;
+            //Cedula Invalida
+        }else{
+            $longitud = strlen($id);
+            $longitudAux = $longitud - 1;
+            $total = 0;
 
-        $query="INSERT INTO users VALUES ($id,'$name','$last','$mail',$phone,'$pass_cod',true)";
-        $result=mysqli_query($conn,$query);
-        if(!$result){
-            die('Query Failed.');
+            for ($i = 0; $i < $longitud; $i = $i + 1){
+                if($i%2 === 0){
+                    $aux = intval(substr($id,$i,1)) * 2;
+                    if($aux > 9){
+                        $aux = $aux - 9;
+                    } 
+                    $total = $total + $aux;
+                }else{
+                    $total = $total +  intval(substr($id,$i,1));
+                }
+            }
+            if ($total % 10 === 0){
+                $total = 0;
+            }else{
+                $total = 10 - $total%10;
+            }
+            if($total === intval(substr($id,$longitud,$longitud))){
+                $query="INSERT INTO users VALUES ($id,'$name','$last','$mail',$phone,'$pass_cod',true)";
+                $result=mysqli_query($conn,$query);
+                echo 405;
+                //Cedula valida
+            }else{
+                echo 406;
+                //Cedula invalida
+            }
         }
-        echo 'Usuario Registrado';
     }
 ?>
--- src/app.js
+++ src/app.js
@@ -7,28 +7,19 @@ $(document).ready(function() {
             mail_user: $('#emailch').val(),
             phone_user: $('#numberch').val(),
             pass_user: $('#passch').val()
-        }; 
-        $.post('backend/reg.php', regdata, function (response) {
-            $('#checkform').trigger('reset');
-            $('#checkform').css('display','none');
-            $('#ch__confirm').css('display','flex');
-        });
-        e.preventDefault();
-    });
 
-    $('#loginform').submit(function(e) {
-        const lgdata = {
-            id_user: $('#cilg').val(),
-            pass_user: $('#passlg').val()
         }; 
-
-        $.get('backend/login.php', lgdata, function (response) {
-            $('#loginform').trigger('reset');
-            if(!response){
-                alert('Usuario o contraña incorrecta');
+        
+        $.post('backend/reg.php', regdata, function (data) {
+            if( data == 406){
+                $('#checkform').trigger('reset');
+                $('#lol').html("Cédula inválida");
             }else{
-                alert('Bienvenido');
-                $(location).attr('href','public/principal.html');
+                $('#lol').hide();
+                $('#checkform').trigger('reset');
+                $('#checkform').css('display','none');
+                $('#ch__confirm').css('display','flex');
+                $('#ch_confirm').html(data);
             }
         });
         e.preventDefault();
